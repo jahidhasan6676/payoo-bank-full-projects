19945aa8d1b0068092f6706902a8a060f54caaa4
jahidhasan6676 <jh18186676@gmail.com>
2024-09-20 15:37:50 +0600

    added the login page

---
+++ js/index.js
@@ -0,0 +1,13 @@
+document.getElementById('login-btn').addEventListener('click', function(event){
+    event.preventDefault();
+    const mobileNumber = getInputFieldValueById('input-mobile-number');
+    const pinPassword = getInputFieldValueById('input-pin-number');
+    
+    
+    if(mobileNumber === 18 && pinPassword === 1234){
+       window.location.href = '/home.html';
+    }
+    else{
+        alert('wrong number and pin');
+    }
+})
---
+++ js/utilities.js
@@ -0,0 +1,6 @@
+// login section
+function getInputFieldValueById(id){
+    const inputValue = document.getElementById(id).value;
+    const inputNumber = parseFloat(inputValue);
+    return inputNumber;
+}
