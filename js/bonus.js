db9bd40fb2b0e871912ae81bb87ab7d7c85a0014
jahidhasan6676 <jh18186676@gmail.com>
2024-09-21 12:59:33 +0600

    added the correction

--- js/bonus.js
+++ js/bonus.js
@@ -1,10 +1,14 @@
 document.getElementById('bonus-btn').addEventListener('click', function(event){
     event.preventDefault();
     const bonusCoupon = getInputFieldValueById('input-bonus-coupon');
+     
+   const code = 76;
+    const bonus = 100;
+    const bonusNumber = parseFloat(bonus);
     
-    if(bonusCoupon === 1000){
+    if(bonusCoupon === code){
         const balance = getInputTextValueById('current-balance');
-        const newBalance = balance + bonusCoupon;
+        const newBalance = balance + bonusNumber;
         document.getElementById('current-balance').innerText = newBalance;
 
     }
